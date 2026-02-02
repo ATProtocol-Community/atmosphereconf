d0f7138f8f933e77bf3a082c6a9d24e84bb44358
Essential Randomness <essentialrandomn3ss@gmail.com>
2026-02-02 14:50:57 -0800

    improve css and do collection for speakers

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 import { glob } from "astro/loaders";
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 import { parseInline } from "marked";
 
 export const collections = {
@@ -8,6 +8,73 @@ export const collections = {
     schema: z.object({
       Q: z.string().transform((question) => parseInline(question)),
       position: z.number(),
+    }),
+  }),
+  agenda: defineCollection({
+    loader: async () => [
+      {
+        id: "1",
+        time: "9:00 AM",
+        title: "Registration & Breadcrumbs",
+        speaker: null,
+      },
+      {
+        id: "2",
+        time: "10:00 AM",
+        title: "Opening Keynote: The Future of Honking",
+        speaker: "honker-mcwaddle",
+      },
+      {
+        id: "3",
+        time: "11:00 AM",
+        title: "Building on the Gaggle Protocol",
+        speaker: "gander-featherton",
+      },
+      { id: "4", time: "12:00 PM", title: "Pond Break", speaker: null },
+      {
+        id: "5",
+        time: "1:30 PM",
+        title: "Identity and Authentication in Goose Networks",
+        speaker: "gosling-rivers",
+      },
+      {
+        id: "6",
+        time: "2:30 PM",
+        title: "Scaling V-Formation Networks",
+        speaker: "migratus-wingsworth",
+      },
+      {
+        id: "7",
+        time: "3:30 PM",
+        title: "Closing Honks & Waddling",
+        speaker: null,
+      },
+    ],
+    schema: z.object({
+      time: z.string(),
+      title: z.string(),
+      speaker: reference("speakers").nullable(),
+    }),
+  }),
+  speakers: defineCollection({
+    loader: glob({ pattern: "**/*.yaml", base: "./src/content/speakers" }),
+    schema: z.object({
+      name: z.string(),
+      title: z.string(),
+      company: z.string(),
+      bio: z.string(),
+    }),
+  }),
+  sponsors: defineCollection({
+    loader: async () => [
+      { id: "goose-labs", name: "Goose Labs" },
+      { id: "flock-foundation", name: "The Flock Foundation" },
+      { id: "honk-industries", name: "Honk Industries" },
+      { id: "waddle-works", name: "Waddle Works" },
+      { id: "feathersoft", name: "Feathersoft" },
+    ],
+    schema: z.object({
+      name: z.string(),
     }),
   }),
 };
